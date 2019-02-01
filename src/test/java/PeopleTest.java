fb54f2657ea0bb40bdf2fcd948be2a28eadb0f84
IVLevchenko <Ai5350ea2292!>
2019-02-01 10:39:43 +0200

    library RestAssured +

--- src/test/java/PeopleTest.java
+++ src/test/java/PeopleTest.java
@@ -1,26 +1,19 @@
-import models.PeopleDescription;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.Assert;
 import org.junit.Test;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Arrays;
 
 
-public class PeopleTest extends PeopleDescription{
+public class PeopleTest {
 
     @Test
     public void test() {
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
-        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-        PeopleDescription response = restTemplate.getForObject("https://swapi.co/api/people/1/", PeopleDescription.class);
-        System.out.println(response);
-        Assert.assertEquals("Shlyapa", getName());
+        RestAssured.given()
+                .baseUri("https://swapi.co")
+                .basePath("/api/people/1/")
+                .accept(ContentType.JSON)
+                .when().get()
+                .then().extract().response()
+                .prettyPrint();
     }
 }
