c1e3caa8b9375eb3ed4bb10d7fae43107382a477
IVLevchenko <Ai5350ea2292!>
2019-02-01 10:47:57 +0200

    library RestAssured 1.2

--- src/test/java/PeopleTest.java
+++ src/test/java/PeopleTest.java
@@ -1,5 +1,6 @@
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -7,7 +8,7 @@ import org.junit.Test;
 public class PeopleTest {
 
     @Test
-    public void test() {
+    public void positiveTest() {
         RestAssured.given()
                 .baseUri("https://swapi.co")
                 .basePath("/api/people/1/")
@@ -15,5 +16,15 @@ public class PeopleTest {
                 .when().get()
                 .then().extract().response()
                 .prettyPrint();
+    }
+
+    @Test
+    public void negativeTest() {
+        RestAssured.given()
+                .baseUri("https://swapi.co")
+                .basePath("/api/people/1/")
+                .accept(ContentType.JSON)
+                .when().get()
+                .then().body("name", Matchers.equalTo("Shlyapa"));
     }
 }
